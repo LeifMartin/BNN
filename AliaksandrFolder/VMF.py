5785bc40f6ad4573775957bd6d58cceb96406c85
Leif-Martin S{ther Sunde <lmsunde@nam-shub-02.uio.no>
2023-01-13 13:22:37 +0100

    Eureka! There was a dimensionality error afterall. Since by constricting the number of nodes in each layer to like 3 the performance is greatly increased. I suspect we have used the vMF up until this point as if if it was all to all nodes, when in actuality it is one to all or all to one.

--- AliaksandrFolder/VMF.py
+++ AliaksandrFolder/VMF.py
@@ -362,8 +362,10 @@ class BayesianLinearLast(nn.Module):
         self.bias_rho = nn.Parameter(torch.Tensor(out_features).uniform_(-5, -4), requires_grad=True)
         self.bias = Gaussian(self.bias_mu, self.bias_rho)
         # Prior distributions
+        #self.weight_prior = HypersphericalUniform(out_features*in_features,DEVICE)
         self.weight_prior = ScaleMixtureGaussian(PI, SIGMA_1, SIGMA_2)
         self.bias_prior = ScaleMixtureGaussian(PI, SIGMA_1, SIGMA_2)
+        #self.bias_prior = HypersphericalUniform(out_features*in_features,DEVICE)#
         self.log_prior = 0
         self.log_variational_posterior = 0
 
@@ -385,18 +387,21 @@ class BayesianLinearLast(nn.Module):
 
 
 class BayesianLinear(nn.Module):
-    def __init__(self, in_features, out_features):
+    def __init__(self, in_features, out_features, w_mu = None, b_mu=None):
         super().__init__()
+        if (w_mu == None or b_mu == None):
+            w_mu = torch.Tensor(out_features*in_features).uniform_(-0.2, 0.2)#In the Gaussian mu's (out,in) is the dimension, not out*in..
+            w_mu = b_mu=torch.Tensor(out_features).uniform_(-0.2, 0.2)
         self.in_features = in_features
         self.out_features = out_features
         # Weight parameters
-        self.weight_mu = nn.Parameter(torch.Tensor(out_features*in_features).uniform_(-0.2, 0.2), requires_grad=True).to(DEVICE)
+        self.weight_mu = nn.Parameter(w_mu, requires_grad=True).to(DEVICE)
         self.weight_rho = nn.Parameter(torch.Tensor(1).uniform_(1,10), requires_grad=True).to(DEVICE)
         self.weight =  vMF(self.weight_mu, logkappa=self.weight_rho, x_dim=out_features * in_features)
         # Bias parameters
-        self.bias_mu = nn.Parameter(torch.Tensor(out_features).uniform_(-0.2, 0.2), requires_grad=True)
-        self.bias_rho = nn.Parameter(torch.Tensor(out_features).uniform_(-5, -4), requires_grad=True)
-        self.bias = Gaussian(self.bias_mu, self.bias_rho)
+        self.bias_mu = nn.Parameter(b_mu, requires_grad=True)
+        self.bias_rho = nn.Parameter(torch.Tensor(1).uniform_(1,10), requires_grad=True)
+        self.bias = vMF(self.bias_mu, logkappa=self.bias_rho, x_dim = out_features)
         # Prior distributions
         self.weight_prior = HypersphericalUniform(out_features*in_features,DEVICE)
         self.bias_prior = HypersphericalUniform(out_features*in_features,DEVICE)
@@ -418,20 +423,37 @@ class BayesianLinear(nn.Module):
 
         return F.linear(input, weight.reshape((self.in_features,self.out_features)).T, bias)
 
-
 class BayesianNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, w_mu1 = None, w_mu2 = None, w_mu3 = None, b_mu1=None, b_mu2=None, b_mu3=None, l1=(256, 400), l2=(400, 600), l3=(600, 5), VD='Gaussian', BN='batchnorm'):
         super().__init__()
-        self.l1 = BayesianLinearLast(256, 400)
-        self.l2 = BayesianLinearLast(400, 600)
-        self.l3 = BayesianLinearLast(600, 5)
+        l1_in, l1_out = l1
+        l2_in, l2_out = l2
+        l3_in, l3_out = l3
+        self.BN = BN
+        if (VD == 'vmf'):
+            self.l1 = BayesianLinear(l1_in, l1_out, w_mu1, b_mu1)
+            self.l2 = BayesianLinear(l2_in, l2_out, w_mu2, b_mu2)
+            self.l3 = BayesianLinear(l3_in, l3_out, w_mu3, b_mu3)
+            #self.l3 = BayesianLinearLast(l3_in, l3_out)
+        else:
+            self.l1 = BayesianLinearLast(l1_in, l1_out)
+            self.l2 = BayesianLinearLast(l2_in, l2_out)
+            self.l3 = BayesianLinearLast(l3_in, l3_out)
 
+    
     def forward(self, x, sample=False):
-        x = x.view(-1, 256)
-        x = F.relu(self.l1(x, sample))
-        x = F.relu(self.l2(x, sample))
-        x = F.log_softmax(self.l3(x, sample), dim=1)#self.l3(x, sample) 
+        if (self.BN=='batchnorm'):
+            x = x.view(-1, 256)
+            x = F.relu(self.l1(x, sample))
+            x = F.relu(self.l2(x, sample))
+            x = F.log_softmax(self.l3(x, sample), dim=1)#self.l3(x, sample) 
+        else: 
+            x = x.view(-1, 256)
+            x = F.batch_norm(F.relu(self.l1(x, sample)))
+            x = F.batch_norm(F.relu(self.l2(x, sample)))
+            x = F.log_softmax(self.l3(x, sample), dim=1)#self.l3(x, sample) 
         return x
+            
 
     def log_prior(self):
         return self.l1.log_prior \
